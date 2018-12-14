10556dde65d8dca410512f5daa0f70876d5bc986
JUN JIE NAN <nanjj@cn.ibm.com>
2018-12-14 11:54:46 +0800

    Added toilet and remove comment

--- figlet.el
+++ figlet.el
@@ -5,12 +5,13 @@
 ;;; customize the figlet-font-dir variable below to point to your
 ;;; figlet font directory.
 ;;;
-;;; M-x figlet      to get a figlet comment in standard font.
+;;; M-x figlet      to get a figlet in standard font.
 ;;; C-u M-x figlet  to be asked for the font first.
 ;;; M-x banner      for an old-fashioned banner font.
+;;; M-x toilet      to get a toilet in standard font.
 ;;;
-;;; These functions use comment-region to wrap the figlet output
-;;; in comments.
+;;; If you want to wrap the figlet output in comments:
+;;; M-x figlet-comment-region
 ;;;
 
 ;;;  _____ ___ ____ _      _     ____  _          __  __
@@ -20,7 +21,7 @@
 ;;; |_|   |___\____|_|\___|\__| |____/ \__|\__,_|_| |_|
 
 
-(defconst figlet-font-dir "/usr/share/figlet")
+;; (defconst figlet-font-dir "/usr/share/figlet")
 (defconst figlet-font-file-regexp "\\.flf$")
 (defconst figlet-match-font-name-regexp "^\\([^.]*\\)\\.flf$")
 
@@ -28,45 +29,66 @@
   (string-match figlet-match-font-name-regexp filename)
   (match-string 1 filename))
 
-(defun figlet-font-names ()
+(defun figlet-font-names (program)
   (mapcar 'figlet-font-name-for-file
-	  (directory-files figlet-font-dir nil figlet-font-file-regexp)))
+	  (directory-files (figlet-font-dir program) nil figlet-font-file-regexp)))
 
-(defun read-figlet-font (prompt)
-  (let* ((figlet-fonts (figlet-font-names))
+(defun figlet-font-dir (program)
+  (string-trim
+   (with-output-to-string
+     (call-process program nil standard-output nil "-I" "2"))))
+
+(defun read-figlet-font (program prompt)
+  (let* ((figlet-fonts (figlet-font-names program))
 	 (font-alist (mapcar (lambda (x) (list x)) figlet-fonts)))
     (completing-read prompt font-alist)))
 
 (defun call-figlet (font string)
+  (figlet-call-process "figlet" font string))
+
+(defun call-toilet (font string)
+  (figlet-call-process "toilet" font string))
+
+(defun figlet-call-process (program font string)
   (push-mark)
-  (call-process "figlet" nil (current-buffer) nil
+  (call-process program nil (current-buffer) nil
 		"-f" (if (null font) "standard" font)
 		string
 		)
   (exchange-point-and-mark))
 
-(defun figlet-block-comment-region ()
-  (comment-region (region-beginning) (region-end)
-		  (if (member major-mode
-			      '(emacs-lisp-mode
-				lisp-mode
-				scheme-mode))
-		      3			; 3 semicolons for lisp
-		    nil)
-		  ))
+(defun figlet-comment-region ()
+  (interactive
+   (comment-region
+    (region-beginning) (region-end)
+	(if (member major-mode
+			    '(emacs-lisp-mode
+				  lisp-mode
+				  scheme-mode))
+		3			; 3 semicolons for lisp
+	  nil))))
 
 (defun figlet (s &optional font)
   (interactive
    (if current-prefix-arg
        (let
-	   ((font (read-figlet-font "Font: "))
+	   ((font (read-figlet-font "figlet" "Font: "))
 	    (text (read-string "FIGlet Text: ")))
 	 (list text font))
      (list (read-string "FIGlet Text: ") nil)))
   (save-excursion
-    (call-figlet font s)
-    (figlet-block-comment-region)
-    ))
+    (call-figlet font s)))
+
+(defun toilet (s &optional font)
+  (interactive
+   (if current-prefix-arg
+       (let
+	   ((font (read-figlet-font "toilet" "Font: "))
+	    (text (read-string "Toilet Text: ")))
+	 (list text font))
+     (list (read-string "Toilet Text: ") nil)))
+  (save-excursion
+    (call-toilet font s)))
 
 (defun banner (s)
   (interactive "sBanner Text: ")
